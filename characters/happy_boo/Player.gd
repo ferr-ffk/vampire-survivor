8996f440ab4310504bed91e2bffd8eb248d28a42
Fernando Freitas <freitax.li@gmail.com>
2024-02-23 13:59:24 -0300

    Criação de personagem de jogador

---
+++ characters/happy_boo/Player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+const VELOCIDADE = 600
+var character
+
+func _ready():
+	character = %HappyBoo
+	character.play_idle_animation()
+	
+func _physics_process(delta):
+	# necessário para definir os botões de movimento
+	# obtem as strings definidas no input map do projeto
+	# retorna o vetor de direção (x positivo, y positivo, etc)
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	velocity = direction * VELOCIDADE
+	
+	move_and_slide()
+	
+	if velocity.length() > 0.0:
+		character.play_walk_animation()
+	else:
+		character.play_idle_animation()
+
+func _process(delta):
+	pass
